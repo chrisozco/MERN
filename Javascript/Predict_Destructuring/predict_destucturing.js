4b3e7bbaf956675492157f2f4f5f1003c644b0c9
Chris Orozco <chrisorozco15@yahoo.com>
2022-09-01 11:56:02 -0700

    first commit

---
+++ Javascript/Predict_Destructuring/predict_destucturing.js
@@ -0,0 +1,75 @@
+//First Problem Predicting
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//Tesla
+//Mercedes
+
+//Second Predict Problem
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//N/A name was changed to 'other name'
+//Elon
+
+//Third Practice Problem
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//12345
+//N/A
+
+//Fourth Practice Problem
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//False
+//True
+
+//Fifth Pactice Problem
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//Value
+//[1, 5, 1, 8, 3, 3]
+//1
+//5
